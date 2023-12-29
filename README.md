4f1b7d62f0362297a2245f60dd9e7e85abfb8735
John West <john.sheffield.west@gmail.com>
2023-12-29 00:05:38 -0500

    Clean up and expand the example with a non-toy dataset

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 A bare-bones implmentation of the self-organizing map algorithm—and a way to visualize it.
 
-
 ## How to use it.
 
 If you're working in just a JavaScript environment, this should work just fine.
@@ -18,15 +17,12 @@ const som = new SOM(config);
 som.train(data);
 ```
 
-Here are the config options you can pass:
-
-- `dimensions`: (integer, required) the number of dimensions in the data that you're trying to pass through the SOM.
-- `height`: (integer, required) the length fo the SOM along the y-axis.
-- `iterations`: (integer, optional--defaults to `1000`) the number of iterations to run during training, which is useful even if you're planning to iterate manually since it controls the decay in the radius and learning rate.
-- `learningRate` (float, optional--defaults to `0.2`) controls how quickly the SOM adapts to training data.
-- `radius` (float, optional--defaults to `1.0`) controls how much of the SOM is updated to training data.
-- `randomSeed` (integer, optional--defaults to `42`) controls the random initialization of neuron weights across the som.
-- `shouldDecay` (boolean, optional--default to `true`) instructs the SOM to decay the learning rate and radius as training progresses.
-- `width`: (integer) the length fo the SOM along the x-axis.
-
 For an example of its usage in a Svelte context, pleas see `/src/routes/+page.svelte`.
+
+## The example.
+
+Run `npm run dev -- --open` to see the example application, which shows a self-organizing map of senators in the 117th congress. The vectors are descriptions of their votes.
+
+The data for the example is drawn from [Voteview](https://voteview.com) data for the Senate during the 117th Congress.
+
+>Lewis, Jeffrey B., Keith Poole, Howard Rosenthal, Adam Boche, Aaron Rudkin, and Luke Sonnet (2023). Voteview: Congressional Roll-Call Votes Database. https://voteview.com/
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,131 +1,285 @@
 <script>
-import { max, scaleLinear } from "d3";
+import { csvParse, max, scaleLinear } from "d3";
 import { Random, Viz } from "$lib/index.js";
 
-const rando = new Random(42);
+// You'll probably not want to put your CSV data files
+// in the static/ folder, but c'est la vie.
+import rawMembers from "./../../static/members.csv?raw";
+import rawVotes from "./../../static/votes.csv?raw";
 
+// Some outliers that weren't normal senators.
+const excludes = [
+	41904, // Loeffler -- partial.
+	41701, // Harris -- vice president.
+	99912, // Trump -- president.
+	99913, // Biden -- president.
+	49300, // Feinstein -- died.
+];
+
+// Ideally, you'd do this kind of data work not in the browser.
+// I'm doing it here just to demonstrate how it works.
+const members = csvParse(rawMembers);
 const data = [];
-for (let i = 0; i < 1000; i++) {
-	data.push({
-		vector: rando.vector(3).map((d) => d * 255),
-	});
+let votes = [];
+for (const vote of csvParse(rawVotes)) {
+	if (!excludes.includes(+vote.icpsr)) {
+		const extant = data.find((d) => d.icpsr === +vote.icpsr);
+		if (extant) {
+			extant.votes.push({
+				cast_code: +vote.cast_code,
+				rollnumber: +vote.rollnumber,
+			})
+		} else {
+			const senator = members.find((m) => +m.icpsr === +vote.icpsr);
+			data.push({
+				bioname: senator.bioname,
+				icpsr: +vote.icpsr,
+				party: +senator.party_code === 200 ? 'r' : 'd',
+				votes: [{
+					cast_code: +vote.cast_code,
+					rollnumber: +vote.rollnumber,
+				}]
+			})
+		}
+		votes.push(+vote.rollnumber);		
+	}
+}
+votes = [...new Set(votes)].sort((a, b) => a - b);
+for (const datum of data) {
+	// We want a one-hot vector for each vote, merged into a long,
+	// sparse vector, which we'll pass to the SOM.
+	datum.vector = [];
+	for (const rollnumber of votes) {
+		const vote = datum.votes.find((v) => v.rollnumber === rollnumber);
+		for (let i = 0; i <= 9; i++) {
+			datum.vector.push(vote?.cast_code === i ? 1 : 0);
+		}
+	}
+	delete datum.votes;
 }
 
-const height = 500;
-const iterations = 200;
-const width = 500;
-
-const forceConfig = {
-	centerStrength: 1,
-	filterEmpties: true,
-	hideEmpties: false,
-	iterations: 10,
-	manyBodyStrength: -30,
-	spaceStrength: 0,
-}
-
-const somConfig = {
-	dimensions: data[0].vector.length,
-	height: 20,
-	iterations,
-	learningRate: 0.4,
-	radius: 1,
-	randomSeed: 42,
-	shouldDecay: true,
-	width: 20,
-};
-
-let viz;
-
-let bgs = [];
-let edges = [];
-let fgs = [];
-let iteration = 0;
-let nodes = [];
-let state = null;
-
+const bgScale = scaleLinear()
+	.domain([0, 1])
+	.range(["rgba(255,100,100,0.5)", "rgba(100,100,255,0.5)"]);
 const callback = (report) => {
 	edges = report.edges;
 	iteration = report.iteration;
 	nodes = report.nodes;
 	state = report.state;
-
-	const bgScale = scaleLinear()
-		.domain([0, max(nodes.map((n) => n.data.length))])
-		.range(["transparent", "rgba(255,0,0,0.75)"]);
-	const fgScale = scaleLinear()
-		.domain([0, max(nodes.map((n) => n.data.length))])
-		.range(["transparent", "rgba(0,0,255,0.5)"]);
-	bgs = nodes.map((n) => {
-		return {
-			id: `node-bg-${n.id}`,
-			end: "transparent",
-			start: `rgba(${n.neuron[0]},${n.neuron[1]},${n.neuron[2]},0.5)`,
-		};
-	});
-	fgs = nodes.map((n) => {
-		return {
-			id: `node-fg-${n.id}`,
-			end: "transparent",
-			start: "transparent"
-		};
-	});
+};
+const forceConfig = {
+	centerStrength: 1,
+	filterEmpties: false,
+	hideEmpties: false,
+	iterations: 2,
+	manyBodyStrength: -300,
+	spaceStrength: 0.03,
+}
+const somConfig = {
+	dimensions: data[0].vector.length,
+	height: 9,
+	iterations: 50,
+	learningRate: 0.25,
+	radius: 1.25,
+	randomSeed: 42,
+	shouldDecay: true,
+	width: 9,
 };
 
-const background = (node) => {
+let active;
+let background;
+let bgs = [];
+let edges = [];
+let fill;
+let fgs = [];
+let height = 468;
+let learningRate = 0;
+let iteration = 0;
+let nodes = [];
+let radius = 0;
+let ready = false;
+let state = null;
+let viz;
+let width = 468;
+
+$: background = (node) => {
 	return `url(#node-bg-${node.id})`;
 };
-const fill = (node) => {
-	return `url(#node)`;
+$: bgs = nodes.map((n) => {
+	return {
+		id: `node-bg-${n.id}`,
+		end: "transparent",
+		start: bgScale(
+			n.data.filter((n) => n.party === "d").length / n.data.length,
+		),
+	};
+});
+$: fill = (node) => {
+	if (node.data.length > 0) {
+		return `url(#node)`;
+	} else {
+		return 'black'
+	}
 };
-const foreground = (node) => {
-	return `url(#node-fg-${node.id})`;
-};
+$: radius = (node) => {
+	if (node.data.length === 0) {
+		return 3.5;
+	} else if (active && active.id === node.id) {
+		return 14;
+	} else {
+		return 3.5;
+	}
+}
 </script>
 
-<div>
-	<div><p>iteration {iteration}</p></div>
-	<button on:click={() => { viz.run() }}>Run</button>
-	<button on:click={() => { viz.iterate() }}>iterate</button>
-	<svg height={height} width={width}>
-		<g class="defs">
-			<defs>
-				<radialGradient id="node">
-					<stop offset="0%" stop-color="white" />
-					<stop offset="50%" stop-color="white" />
-					<stop offset="100%" stop-color="transparent" />
-				</radialGradient>
-				{#each bgs as bg}
-					<radialGradient id="{bg.id}">
-						<stop offset="0%" stop-color="{bg.start}" />
-						<stop offset="100%" stop-color="{bg.end}" />
-					</radialGradient>
+<main>
+	<div id="controls">
+		<header>
+			<h2>Midsommer</h2>
+			<h1>How the Senate of the 117th congress voted.</h1>
+		</header>
+		<div class="control">
+			<button on:click={() => { viz.run() }}>run</button>
+		</div>
+		<div class="control">
+			<p>iteration {iteration}</p>
+		</div>
+		<div class="control">
+			<p>learning rate {Math.round(state ? state.learningRate * 100 : 0) / 100}</p>
+		</div>
+		<div class="control">
+			<p>radius {Math.round(state ? state.radius * 100 : 0) / 100}</p>
+		</div>
+	</div>
+	<div id="inspector">
+		{#if active}
+			<ul>
+				{#each active.data as senator}
+				<li>
+					<span class="name">{senator.bioname}</span> 
+					<span class="party">({senator.party.toUpperCase()})</span>
+				</li>
 				{/each}
-				{#each fgs as fg}
-					<radialGradient id="{fg.id}">
-						<stop offset="0%" stop-color="{fg.start}" />
-						<stop offset="100%" stop-color="{fg.end}" />
+			</ul>
+		{/if}
+	</div>
+	<div id="viz">
+		<svg height={height} width={width}>
+			<g class="defs">
+				<defs>
+					<radialGradient id="node">
+						<stop offset="0%" stop-color="white" />
+						<stop offset="50%" stop-color="white" />
+						<stop offset="100%" stop-color="transparent" />
 					</radialGradient>
-				{/each}
-			</defs>
-		</g>
-		<Viz
-			bind:this={viz}
-			background={background}
-			callback={callback}
-			data={data}
-			fill={fill}
-			forceConfig={forceConfig}
-			foreground={foreground}
-			height={height}
-			somConfig={somConfig}
-			width={width}
-		/>
-	</svg>
-</div>
+					{#each bgs as bg}
+						<radialGradient id="{bg.id}">
+							<stop offset="0%" stop-color="{bg.start}" />
+							<stop offset="100%" stop-color="{bg.end}" />
+						</radialGradient>
+					{/each}
+					{#each fgs as fg}
+						<radialGradient id="{fg.id}">
+							<stop offset="0%" stop-color="{fg.start}" />
+							<stop offset="100%" stop-color="{fg.end}" />
+						</radialGradient>
+					{/each}
+				</defs>
+			</g>
+			<Viz
+				bind:this={viz}
+				background={background}
+				callback={callback}
+				click={(node) => {active = active && active.id === node.id ? null : node}}
+				data={data}
+				fill={fill}
+				forceConfig={forceConfig}
+				height={height}
+				radius={radius}
+				somConfig={somConfig}
+				width={width}
+			/>
+		</svg>
+	</div>
+</main>
 
 <style>
-	svg {
-		background-color: black;
-	}
+#controls {
+	border: 1px solid white;
+	box-sizing: border-box;
+	height: 60px;
+	left: 15px;
+	position: fixed;
+	padding: 15px;
+	top: 15px;
+	width: calc(500px + 300px + 15px);
+}
+#controls header {
+	float: left;
+}
+#controls header h2 {
+	color: white;
+	font-family: "Helvetica";
+	font-size: 10px;
+	letter-spacing: 3px;
+	line-height: 15px;
+	text-transform: uppercase;
+}
+#controls header h1 {
+	color: white;
+	font-family: "Helvetica";
+	font-size: 15px;
+	font-weight: 800;
+	line-height: 15px;
+}
+#controls .control {
+	float: right;
+	margin-left: 15px;
+}
+#controls .control button {
+	background-color: black;
+	box-sizing: border-box;
+	border: 1px solid white;
+	border-radius: 0px;
+	color: white;
+	cursor: pointer;
+	font-family: "Helvetica";
+	font-size: 10px;
+	font-weight: 400;
+	height: 30px;
+	line-height: 15px;
+	width: 150px;
+}
+#controls .control p {
+	color: white;
+	font-family: "Helvetica";
+	font-size: 10px;
+	height: 30px;
+	line-height: 30px;
+}
+#inspector {
+	border: 1px solid white;
+	box-sizing: border-box;
+	height: 500px;
+	left: 15px;
+	overflow: scroll;
+	padding: 15px;
+	position: fixed;
+	top: calc(15px + 60px + 15px);
+	width: 300px;
+}
+#inspector ul {
+	color: white;
+	margin-left: 15px;
+}
+#viz {
+	border: 1px solid white;
+	box-sizing: border-box;
+	height: 500px;
+	left: calc(300px + 15px + 15px);
+	padding: 15px;
+	position: fixed;
+	top: calc(15px + 60px + 15px);
+	width: 500px;
+}
 </style>
